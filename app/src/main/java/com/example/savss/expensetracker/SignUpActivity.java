ee7267a7a2dbe7cd996b026953be4a990f2f5000
Vijay-Nirmal <pon.vijaynirmal@outlook.com>
2018-02-16 00:28:13 +0530

    Added Sign Up page

---
+++ app/src/main/java/com/example/savss/expensetracker/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.example.savss.expensetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().hide();
+    }
+
+    public void signUpButton_onClick(View view) {
+        //TODO: OTP
+    }
+}
