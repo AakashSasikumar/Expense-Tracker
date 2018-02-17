cd7ab81091d026623c679d63c9550a13abc089c1
Aakash Sasikumar <cb.en.u4cse15301@cb.students.amrita.edu>
2018-02-17 14:05:26 +0530

    added placeholder for dashboard

--- app/src/main/java/com/example/savss/expensetracker/HomeActivity.java
+++ app/src/main/java/com/example/savss/expensetracker/HomeActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -23,17 +24,16 @@ public class HomeActivity extends AppCompatActivity {
                     // selectedFragment = ItemOneFragment.newInstance();
                     break;
                 case R.id.navigation_dashboard:
-                    // selectedFragment = ItemOneFragment.newInstance();
+                    selectedFragment = new DashboardFragment();
                     break;
                 case R.id.navigation_settings:
                     // selectedFragment = ItemOneFragment.newInstance();
                     break;
             }
-            // FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            // transaction.replace(R.id.frame_layout, selectedFragment);
-            // transaction.commit();
-            // return true;
-            return false;
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout, selectedFragment);
+            transaction.commit();
+            return true;
         }
     };
 
@@ -41,7 +41,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
