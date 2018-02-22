5aeb01fc9908b8cc04c2a682e0d400163e9cc5c0
Vijay-Nirmal <pon.vijaynirmal@outlook.com>
2018-02-22 17:14:42 +0530

    Added PieChart for Today expense

--- app/src/main/java/com/example/savss/expensetracker/DashboardFragment.java
+++ app/src/main/java/com/example/savss/expensetracker/DashboardFragment.java
@@ -8,7 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
 public class DashboardFragment extends Fragment {
+
+    private PieChart todayPieChart;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,9 +25,39 @@ public class DashboardFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View dashboardView = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        todayPieChart = dashboardView.findViewById(R.id.todayPieChart);
+        todayPieChart.setRotationEnabled(true);
+        todayPieChart.setHoleRadius(25f);
+        todayPieChart.setTransparentCircleAlpha(128);
+        todayPieChart.setDrawEntryLabels(true);
+
+        Legend legend = todayPieChart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
+
+        LocalDatabaseHelper localDatabaseHelper = new LocalDatabaseHelper(dashboardView.getContext(), null, null, 1);
+        ExpenseData expenseData = localDatabaseHelper.getTodaysExpenses();
+        PieData pieData = new PieData(expenseData.getPieDataSet());
+        todayPieChart.setData(pieData);
+        todayPieChart.invalidate();
+
+        todayPieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                System.out.println(h.toString());
+                System.out.println(h.getY());
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
+
+        return dashboardView;
     }
 
 
--- app/src/main/java/com/example/savss/expensetracker/ExpenseData.java
+++ app/src/main/java/com/example/savss/expensetracker/ExpenseData.java
@@ -1,21 +1,41 @@
 package com.example.savss.expensetracker;
 
+import android.graphics.Color;
+
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ExpenseData {
     private ArrayList<String> categories = new ArrayList<>();
-    private ArrayList<Integer> expenseAmounts = new ArrayList<>();
+    private ArrayList<PieEntry> expenseAmounts = new ArrayList<>();
 
     public void add(String category, int expenseAmount) {
         categories.add(category);
-        expenseAmounts.add(expenseAmount);
+        expenseAmounts.add(new PieEntry(expenseAmount));
     }
 
-    public ArrayList<String> getCategories() {
-        return categories;
-    }
+    public PieDataSet getPieDataSet() {
+        PieDataSet pieDataSet = new PieDataSet(expenseAmounts, "Today's expense");
+        pieDataSet.setSliceSpace(5);
+        pieDataSet.setValueTextSize(12f);
 
-    public ArrayList<Integer> getExpenseAmounts() {
-        return expenseAmounts;
+        ArrayList<Integer> chartColors = new ArrayList<>();
+        Random random = new Random();
+        float[] hsv = new float[3];
+
+        for (PieEntry i : expenseAmounts) {
+            hsv[0] = random.nextFloat() * 359;
+            hsv[1] = 0.5f + (random.nextFloat() * 0.5f);
+            hsv[2] = 0.5f + (random.nextFloat() * 0.5f);
+            chartColors.add(Color.HSVToColor(hsv));
+        }
+
+        pieDataSet.setColors(chartColors);
+
+        return pieDataSet;
     }
 }
