0fa866670f040c2160f81b733d9a0aa43cc604c0
Saumya-Rai <32762940+Saumya-Rai@users.noreply.github.com>
2018-02-26 22:35:43 +0530

    Profile and app management
    swipe tabs implemented. will do functionality now

---
+++ app/src/main/java/com/example/savss/expensetracker/AppSettings.java
@@ -0,0 +1,25 @@
+package com.example.savss.expensetracker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Saumya on 26-02-2018.
+ */
+
+public class AppSettings extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_app_settings, container, false);
+    }
+}
