6f2d216453f52be1d96c50198c62858c80270d2f
Aakash Sasikumar <cb.en.u4cse15301@cb.students.amrita.edu>
2018-02-26 23:54:42 +0530

    working towards add transacion page

--- app/src/main/java/com/example/savss/expensetracker/AddTransactionFragment.java
+++ app/src/main/java/com/example/savss/expensetracker/AddTransactionFragment.java
@@ -7,10 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class AddTransactionFragment extends Fragment {
 
+    private String transactionType = "";
+    private Button income;
+    private Button expense;
+    private TextView typeDisplay;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +24,29 @@ public class AddTransactionFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_transaction, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View addTransactionView = inflater.inflate(R.layout.fragment_add_transaction, container, false);
+        income = addTransactionView.findViewById(R.id.income_button);
+        expense = addTransactionView.findViewById(R.id.expense_button);
+        typeDisplay = addTransactionView.findViewById(R.id.typeDisplay);
+        income.setOnClickListener(incomeOnClickListener);
+        expense.setOnClickListener(expenseOnClickListener);
+        return addTransactionView;
     }
 
+    private View.OnClickListener incomeOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            transactionType = "income";
+            typeDisplay.setText(transactionType.toUpperCase());
+        }
+    };
+
+    private View.OnClickListener expenseOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            transactionType = "expense";
+            typeDisplay.setText(transactionType.toUpperCase());
+        }
+    };
 }
