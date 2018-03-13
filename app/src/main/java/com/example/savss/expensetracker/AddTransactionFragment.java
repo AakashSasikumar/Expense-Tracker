4bcb5903ad17102ca5088e95af5327639cfe76f0
Aakash Sasikumar <cb.en.u4cse15301@cb.students.amrita.edu>
2018-03-13 19:10:58 +0530

    added amount display

--- app/src/main/java/com/example/savss/expensetracker/AddTransactionFragment.java
+++ app/src/main/java/com/example/savss/expensetracker/AddTransactionFragment.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -16,7 +20,10 @@ public class AddTransactionFragment extends Fragment {
     private String transactionType = "";
     private Button income;
     private Button expense;
-    private TextView typeDisplay;
+    private TextView incomeOrExpense;
+    private EditText value;
+    private float defaultTextSize = 80;
+    private float changedTextSize = 80;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +35,9 @@ public class AddTransactionFragment extends Fragment {
         View addTransactionView = inflater.inflate(R.layout.fragment_add_transaction, container, false);
         income = addTransactionView.findViewById(R.id.income_button);
         expense = addTransactionView.findViewById(R.id.expense_button);
-        typeDisplay = addTransactionView.findViewById(R.id.typeDisplay);
+        incomeOrExpense = addTransactionView.findViewById(R.id.income_or_expense);
+        value = addTransactionView.findViewById(R.id.valueDisplay);
+        value.addTextChangedListener(valueTextViewWatcher);
         income.setOnClickListener(incomeOnClickListener);
         expense.setOnClickListener(expenseOnClickListener);
         return addTransactionView;
@@ -37,16 +46,42 @@ public class AddTransactionFragment extends Fragment {
     private View.OnClickListener incomeOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            transactionType = "income";
-            typeDisplay.setText(transactionType.toUpperCase());
+            transactionType = "+";
+            incomeOrExpense.setTextSize(80);
+            incomeOrExpense.setText(transactionType);
         }
     };
 
     private View.OnClickListener expenseOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            transactionType = "expense";
-            typeDisplay.setText(transactionType.toUpperCase());
+            transactionType = "-";
+            incomeOrExpense.setTextSize(100);
+            incomeOrExpense.setText(transactionType);
+        }
+    };
+    private TextWatcher valueTextViewWatcher = new TextWatcher() {
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (isTooLarge()) {
+                changedTextSize  = defaultTextSize - (value.getText().toString().trim().length() - 8) * 6;
+                value.setTextSize(TypedValue.COMPLEX_UNIT_DIP, changedTextSize);
+            }
+            else {
+                value.setTextSize(TypedValue.COMPLEX_UNIT_DIP, defaultTextSize);
+                changedTextSize = defaultTextSize;
+            }
+        }
+        private boolean isTooLarge() {
+            return value.getText().toString().trim().length() > 7;
         }
     };
 }
