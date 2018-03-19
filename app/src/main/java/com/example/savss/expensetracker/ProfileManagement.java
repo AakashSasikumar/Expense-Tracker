d24e07fb76fd84961750f6d6e7d9997b5e737ead
Saumya-Rai <32762940+Saumya-Rai@users.noreply.github.com>
2018-03-19 21:54:08 +0530

    Profile management settings without db connectiity
    just has placeholders

--- app/src/main/java/com/example/savss/expensetracker/ProfileManagement.java
+++ app/src/main/java/com/example/savss/expensetracker/ProfileManagement.java
@@ -5,12 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ViewSwitcher;
+import android.widget.Button;
 
 /**
  * Created by Saumya on 26-02-2018.
  */
 
-public class ProfileManagement extends Fragment{
+public class ProfileManagement extends Fragment {
+
+    private ViewSwitcher viewSwitcher;
+    Button btnNext, btnPrev;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,32 @@ public class ProfileManagement extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile_management, container, false);
+
+
+        View viewapp = inflater.inflate(R.layout.fragment_profile_management,container, false);
+
+        btnNext = (Button) viewapp.findViewById(R.id.changeProfile);
+        btnPrev = (Button) viewapp.findViewById(R.id.updateProfile);
+
+        viewSwitcher = (ViewSwitcher) viewapp.findViewById(R.id.profileViewSwitcher);
+
+        btnNext.setOnClickListener(setViewSwitcherNext);
+        btnPrev.setOnClickListener(setViewSwitcherPrev);
+        return viewapp;
     }
+
+    private View.OnClickListener setViewSwitcherNext = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            viewSwitcher.setDisplayedChild(1);
+            //Toast.makeText(getActivity(), "hello", Toast.LENGTH_SHORT).show();
+        }
+    };
+    private View.OnClickListener setViewSwitcherPrev = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            viewSwitcher.setDisplayedChild(0);
+            //Toast.makeText(getActivity(), "hello", Toast.LENGTH_SHORT).show();
+        }
+    };
 }
