b8711e8fa6ba99c56f6a7b6b8f72343cb7589a51
shantam97 <bhuraria38@gmail.com>
2018-04-09 20:27:17 +0530

    added firebase code for sign up activity

--- app/src/main/java/com/example/savss/expensetracker/SignUpActivity.java
+++ app/src/main/java/com/example/savss/expensetracker/SignUpActivity.java
@@ -3,6 +3,7 @@ package com.example.savss.expensetracker;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Vibrator;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,17 +12,28 @@ import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
 
     Toast toast;
     FirebaseDatabase database;
-
+ private FirebaseAuth firebaseAuth;
+ FirebaseFirestore firebaseFirestore;
+ FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,30 +41,85 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
         toast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
         database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        firebaseAuth=FirebaseAuth.getInstance();
 
     }
 
     public void signUpButton_onClick(View view) {
         // TODO: OTP for Phone Number validation
 
-        EditText yourName = findViewById(R.id.yourName);
-        EditText emailAddress = findViewById(R.id.emailAddress);
-        EditText phoneNumber = findViewById(R.id.phoneNumber);
-        EditText password = findViewById(R.id.password);
+        final EditText yourName = findViewById(R.id.yourName);
+        final EditText emailAddress = findViewById(R.id.emailAddress);
+        final EditText phoneNumber = findViewById(R.id.phoneNumber);
+        final EditText password = findViewById(R.id.password);
         EditText confirmPassword = findViewById(R.id.confirmPassword);
 
         if (isSignUpDetailsValid(yourName, emailAddress, phoneNumber, password, confirmPassword)) {
-            LocalDatabaseHelper localDatabaseHelper = new LocalDatabaseHelper(this, null, null, 1);
+            final LocalDatabaseHelper localDatabaseHelper = new LocalDatabaseHelper(this, null, null, 1);
 
             boolean addUserResult = localDatabaseHelper.tryAddUser(yourName.getText().toString(), emailAddress.getText().toString(), phoneNumber.getText().toString(), password.getText().toString());
 
             if (addUserResult) {
 
-                FirebaseDBHelper.addUser(yourName.getText().toString(), emailAddress.getText().toString(), phoneNumber.getText().toString(), password.getText().toString());
-                Intent toDashboard = new Intent(this, HomeActivity.class);
-                toDashboard.putExtra(LocalDatabaseHelper.COLUMN_ID, localDatabaseHelper.getUserID(emailAddress.getText().toString()));
-                startActivity(toDashboard);
-                displayTosat(R.string.userSuccessfullyAdded);
+                //DatabaseReference myRef = database.getReference("message");
+                //String child=phoneNumber.toString();
+                //myRef=database.getReference("Users").child(child);
+                //myRef.child("Email").setValue(emailAddress);
+                //myRef.child("Name").setValue(yourName);
+                //myRef.child("Number").setValue(phoneNumber);
+                //myRef.child("Password").setValue(password);
+       firebaseAuth.createUserWithEmailAndPassword(emailAddress.getText().toString().trim(),password.getText().toString().trim()).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+//                        Intent intent = new Intent(SignUpActivity.this,HomeActivity.class);
+//                        startActivity(intent);
+//                        finish();
+                        HashMap<String,Object> hashMap=new HashMap<>();
+                        hashMap.put("name",yourName.getText().toString().trim());
+                        hashMap.put("email",emailAddress.getText().toString());
+                        hashMap.put("phonenum",phoneNumber.getText().toString().trim());
+                        hashMap.put("password",password.getText().toString());
+                        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+                        String uid=firebaseUser.getUid();
+                        firebaseFirestore.collection("users").document(phoneNumber.getText().toString()).set(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                                if (task.isSuccessful()){
+                                    Intent intent=new Intent(SignUpActivity.this,HomeActivity.class);
+                                    intent.putExtra(LocalDatabaseHelper.COLUMN_ID, localDatabaseHelper.getUserID(emailAddress.getText().toString()));
+                                    startActivity(intent);
+                                    Toast.makeText(SignUpActivity.this, "success", Toast.LENGTH_SHORT).show();
+                                    finish();
+
+                                }
+                                else {
+                                    Toast.makeText(SignUpActivity.this, "error", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+
+
+                    }
+                    else{
+                        FirebaseAuthException e = (FirebaseAuthException) task.getException();
+                        Toast.makeText(getApplicationContext(), "error" + " " + e.getMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+
+
+                }
+            });
+
+//                Intent toDashboard = new Intent(this, HomeActivity.class);
+
+//                startActivity(toDashboard);
+//                displayTosat(R.string.userSuccessfullyAdded);
             }
             else {
                 displayTosat(R.string.userAlreadyExistError);
